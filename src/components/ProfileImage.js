30fa6f6ca5b7a370c31aa58b140991caae416853
CristianMP04 <christian.marti.pernin@gmail.com>
2022-07-11 18:26:19 +0200

    begin ite 6
    begin ite 6

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,10 @@
+function profileImage(props) {
+    
+    return <img
+                src={props.image}
+                className="profile"
+                alt="profile"
+            />
+}
+
+export default profileImage
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,20 @@
-function Tweet() {
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+
+function Tweet(props) {
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={props.tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+        <User userData={props.tweet.user} />
 
-          <span className="timestamp">Nov 30, 2020</span>
+        <Timestamp time={props.tweet.timestamp} />
         </div>
 
         <p className="message">
